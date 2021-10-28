9d44a97931fd804b4669f019899d29fa54a98ef1
manimovassagh <mani.mgh@gmail.com>
2021-10-28 15:52:58 +0200

    make some components to practice

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template >
+  <div>
+    <h1>This is a Footer of My App</h1>
+    <input type="text" v-model="inputData" />
+    <h1>{{ inputData }}</h1>
+    <h1 @click="changeName">{{ name }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "mani",
+      alternateName: "some",
+      inputData: "",
+    };
+  },
+  methods: {
+    changeName() {
+      if (this.name == "mani") {
+        this.name = this.alternateName;
+      } else {
+        this.name = "mani";
+      }
+    },
+  },
+};
+</script>
+<style >
+</style>
